5f3e036b41492e5be39b6837213e9a61685522e1
Anshul <anshulbirla@gmail.com>
2020-08-06 16:07:06 -0700

    added user input

--- build/Navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anshul/Coding/MicroFRED/src/Navigation
-# Build directory: /home/anshul/Coding/MicroFRED/build/Navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
