838293609db9c6a7a9cd0b43ee5c4083350dc6d2
agerami20 <agerami20@gmail.com>
2020-08-07 10:50:08 -0700

    teleop_subscriber finished

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ava/MicroFRED/src
-# Build directory: /home/ava/MicroFRED/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("Navigation")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ava/MicroFRED/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ava/MicroFRED/devel;/opt/ros/melodic'.split(';') if '/home/ava/MicroFRED/devel;/opt/ros/melodic' != '' else []
