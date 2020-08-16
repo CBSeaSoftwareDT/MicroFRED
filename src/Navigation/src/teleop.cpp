53c7a68c9204a3e819593a3de6281315abf48118
Anshul <anshulbirla@gmail.com>
2020-08-15 17:13:29 -0700

    changed the user_input class to use new messages

--- src/Navigation/src/teleop.cpp
+++ src/Navigation/src/teleop.cpp
@@ -1,29 +1,39 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <Navigation/Joystick.h>
 #include <iostream>
 #include <string>
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "user_input_node");
 	ros::NodeHandle n;
-	ros::Publisher publisher = n.advertise<std_msgs::Float64>("user_input", 1);
-	std_msgs::Float64 input;
+	ros::Publisher publisher = n.advertise<Navigation::Joystick>("user_input", 1);
+	Navigation::Joystick user_input;
 	
-	input.data = 0;
+	int input;
+	user_input.joystick_horizontal = 0;
+	user_input.joystick_vertical = 0;
 
 	ros::Rate naptime(1.0);
 
 	while(ros::ok())
 	{
-		std::cout << "Enter float between -90 and 90: ";
-		float float_input;
-		std::cin >> float_input;
-		if(float_input > 90 || float_input < -90 )
+		std::cout << "Enter any integer between 0 and 95 for the horizontal joystick reading: ";
+		
+		std::cin >> input;
+		if(input > 95 || input < 0 )
 			std::cout << "Invalid input";
-		else {
-			input.data = float_input;
-			publisher.publish(input);
-		}
+		else 
+			user_input.joystick_horizontal = input;
+		
+		std::cout << "Enter any integer between -95 and 95 for the horizontal joystick reading: ";
+		std::cin >> input;
+		if(input < -95 || input >95)
+			std::cout << "Invalid input";
+		else 
+			user_input.joystick_vertical = input;
+		
+		publisher.publish(user_input);
 
 		naptime.sleep();
 	}
